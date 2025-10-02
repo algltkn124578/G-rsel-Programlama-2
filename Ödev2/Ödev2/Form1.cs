c3186bca245b2fdf294c62116f58fad8fb222059
algltkn124578 <aligultekin385@gmail.com>
2025-10-02 11:54:32 +0300

    Mouse Ödevi

---
+++ Ödev2/Ödev2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ödev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool sürükleme;
+        int mouseX, mouseY;
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sürükleme)
+            {
+                button1.Left += e.X -(button1.Width/2);
+                button1.Top += e.Y-(button1.Height/2);
+
+            }
+
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+                       
+            sürükleme = true;
+            mouseX = e.X ;
+            mouseY = e.Y;
+
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            sürükleme = false;
+        }
+    }
+}
+
